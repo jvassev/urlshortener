c482f7f2eb28726de40a874fce42715b4b5a79e3
Julian Vassev <jvassev@gmail.com>
2012-08-04 16:29:44 +0300

    fix compilation

--- src/main/java/jvassev/urlshortener/ShortenerServlet.java
+++ src/main/java/jvassev/urlshortener/ShortenerServlet.java
@@ -7,12 +7,11 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import jvassev.urlshortener.impl.RedisBackedUrlShortener;
+
 import org.apache.commons.io.IOUtils;
-import org.apache.commons.lang.StringUtils;
 
 import redis.clients.jedis.Jedis;
-
-import jvassev.urlshortener.impl.RedisBackedUrlShortener;
 
 public class ShortenerServlet extends HttpServlet {
 
